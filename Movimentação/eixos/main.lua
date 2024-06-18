bca279896415e04828b5d6e49598e67856d22a01
Carlos Daniel Gomes Santana <cdgomessantana@gmail.com>
2024-06-18 20:23:44 -0300

    Adicionado um limite para a movimentação do quadrado dentro da grade e um ajuste simples do tamanho relacionado a grade

--- Movimentação/eixos/main.lua
+++ Movimentação/eixos/main.lua
@@ -4,6 +4,7 @@ function love.load()
     RED_COLOR = {1, 0, 0}
     DIVISOR = {5, 10, 20, 40, 50, 100, 200}
     INDEX_DIVISOR = 1
+    SQUARE_X, SQUARE_Y = 0, 0
 end
 
 function love.wheelmoved(x, y)
@@ -14,7 +15,20 @@ function love.wheelmoved(x, y)
     end
 end
 
+function love.keypressed( key, scancode, isrepeat )
+    if scancode == "right" and SQUARE_X < WIDTH - SQUARE_X/2 then
+        SQUARE_X = SQUARE_X + DIVISOR[INDEX_DIVISOR]
+    elseif scancode == "left" and 0 < SQUARE_X  then
+        SQUARE_X = SQUARE_X - DIVISOR[INDEX_DIVISOR]
+    elseif scancode == "down" and SQUARE_Y < HEIGHT - SQUARE_Y/2 then
+        SQUARE_Y = SQUARE_Y + DIVISOR[INDEX_DIVISOR]
+    elseif scancode == "up" and 0 < SQUARE_Y then
+        SQUARE_Y = SQUARE_Y - DIVISOR[INDEX_DIVISOR]
+    end
+ end
+
 function love.draw()
+    love.graphics.setLineWidth(1)
     love.graphics.setColor(WHITE_COLOR)
 
     for point = 0, WIDTH, DIVISOR[INDEX_DIVISOR] do
@@ -34,7 +48,13 @@ function love.draw()
 
     love.graphics.setColor(RED_COLOR)
 
-    love.graphics.rectangle("line", 0, 0, 60, 60)
+    love.graphics.setLineWidth(3)
+    love.graphics.rectangle(
+        "line",
+        SQUARE_X, SQUARE_Y,
+        DIVISOR[INDEX_DIVISOR],
+        DIVISOR[INDEX_DIVISOR]
+    )
 
 end
 
